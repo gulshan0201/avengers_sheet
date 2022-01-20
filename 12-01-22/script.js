4a90fc037ae165e461a5f796ec7c4f39bd44bafc
gulshan0201 <84889171+gulshan0201@users.noreply.github.com>
2022-01-20 19:27:29 +0530

    Add files via upload

---
+++ 12-01-22/script.js
@@ -0,0 +1,11 @@
+var data = 0;
+document.getElementById("root").innerText = data;
+function decrement() {
+  data = data - 1;
+  document.getElementById("root").innerText = data;
+}
+
+function increment() {
+  data = data + 1;
+  document.getElementById("root").innerText = data;
+}
