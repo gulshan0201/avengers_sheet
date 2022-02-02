c53194ccfab5f36e1a6a49c16691afa670d9fa03
gulshan0201 <84889171+gulshan0201@users.noreply.github.com>
2022-02-02 14:39:08 +0530

    Add files via upload

---
+++ 31-01-22/script.js
@@ -0,0 +1,11 @@
+var arr = [2,7,11,15,14,5,9,6];
+var target = 19;
+for(let i = 0 ; i < arr.length ; i++){
+    for (let j = i + 1; j <= arr.length - 1; j++){
+        if(arr[i] + arr[j] == target){
+            console.log([i,j]);
+            document.write("The index of array is " + [i,j]);
+        }
+    }
+}
+
